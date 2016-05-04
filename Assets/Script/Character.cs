bc640b2f37e446a9540b687c0ab00327b89159aa
kimilkwon <kimilgwon@gmail.com>
2016-05-04 22:45:32 +0900

    캐릭터 이동/카메라 이동/타일 배치

---
+++ Assets/Script/Character.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour {
+
+    // Use this for initialization
+
+    int speed = 5; //스피드 
+    void Update()
+    {
+        C_Move();
+
+    }
+
+    void C_Move()
+    {
+
+        float xMove = Input.GetAxis("Horizontal") * speed * Time.deltaTime; //x축으로 이동할 양 
+        float yMove = Input.GetAxis("Vertical") * speed * Time.deltaTime; //y축으로 이동할양 
+        this.transform.Translate(new Vector3(xMove, yMove, 0));  //이동
+    }
+  
+    
+}
